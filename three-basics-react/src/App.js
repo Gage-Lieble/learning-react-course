ded59c76e5c27d914b0391c8bf239eaa699f1361
Gage Lieble <gagelieble@hotmail.com>
2022-12-23 14:23:00 -0800

    Created first practice app, section 3

---
+++ three-basics-react/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Basics</h1>
+      <p>This is section 3 from the udemy course I'm following!</p>
+    </div>
+  );
+}
+
+export default App;
