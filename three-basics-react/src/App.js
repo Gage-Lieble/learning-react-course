e1a5040b0e5b5836a1cc2104b3566027651df219
Gage Lieble <gagelieble@hotmail.com>
2022-12-23 15:52:47 -0800

    Splitting components

--- three-basics-react/src/App.js
+++ three-basics-react/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -12,10 +12,9 @@ function App() {
     <div className="App">
       <h1>React Basics</h1>
       <p>This is section 3 from the udemy course I'm following!</p>
-      <ExpenseItem title={expenses[0].title}amount={expenses[0].amount}date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title}amount={expenses[1].amount}date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title}amount={expenses[2].amount}date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title}amount={expenses[3].amount}date={expenses[3].date}/>
+      <Expenses array={expenses}/>
+      
+      
       
     </div>
   );
---
+++ three-basics-react/src/components/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem"
+import './Expenses.css'
+import Card from "./Card"
+function Expenses(props){
+    return (
+        <Card className="expenses">
+            <ExpenseItem title={props.array[0].title}amount={props.array[0].amount}date={props.array[0].date}/>
+            <ExpenseItem title={props.array[1].title}amount={props.array[1].amount}date={props.array[1].date}/>
+            <ExpenseItem title={props.array[2].title}amount={props.array[2].amount}date={props.array[2].date}/>
+            <ExpenseItem title={props.array[3].title}amount={props.array[3].amount}date={props.array[3].date}/>
+        </Card>
+    )
+}
+
+export default Expenses
