83dbbd382f5780a83598a17334a11de775062f6e
Gage Lieble <gagelieble@hotmail.com>
2022-12-23 14:36:44 -0800

    Created first custom comp

--- three-basics-react/src/App.js
+++ three-basics-react/src/App.js
@@ -1,10 +1,11 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <h1>React Basics</h1>
       <p>This is section 3 from the udemy course I'm following!</p>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ three-basics-react/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+
+function ExpenseItem(){
+    return (
+    <div>
+        <div>12/23/2022</div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$200.00</div>
+        </div>
+    </div>
+            );
+}
+
+export default ExpenseItem;
