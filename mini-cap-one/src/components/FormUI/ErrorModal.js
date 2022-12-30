31552fd5e8299641240155d67fe57fbbe320f4b7
Gage Lieble <gagelieble@hotmail.com>
2022-12-29 18:43:41 -0800

    Intro to portals

--- mini-cap-one/src/components/FormUI/ErrorModal.js
+++ mini-cap-one/src/components/FormUI/ErrorModal.js
@@ -1,5 +1,21 @@
+import ReactDom from 'react-dom'
 import './ErrorModal.css'
 
+
+const ModalOverlay = (props) => {
+    return(
+        <div className='modal-wrap'>
+            <div>
+                <h3 className='error-title'>Invalid input!</h3>
+            </div>
+            <div>
+                <h4 className='error-msg'>{props.msg}</h4>
+            </div>
+
+            <button className='error-close' onClick={() => {props.showModal('false'); props.ageHook('')}}>close</button>
+        </div>
+    )
+}
 const ErrorModal = (props) => {
 
     let errorMsg = 'Please enter a value for each box (no empty values)'
@@ -9,16 +25,9 @@ const ErrorModal = (props) => {
         
     }
     return(
-        <div className='modal-wrap'>
-            <div>
-                <h3 className='error-title'>Invalid input!</h3>
-            </div>
-            <div>
-                <h4 className='error-msg'>{errorMsg}</h4>
-            </div>
-
-            <button className='error-close' onClick={() => {props.showModal('false'); props.ageHook('')}}>close</button>
-        </div>
+        <>
+        {ReactDom.createPortal(<ModalOverlay msg={errorMsg} showModal={props.showModal} ageHook={props.ageHook}/>, document.getElementById('modal-root'))}
+        </>
     )
 }
 
