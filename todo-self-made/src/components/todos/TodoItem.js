068c50e289154f612fa7bc7aee4d4613edddeaf6
Gage Lieble <gagelieble@hotmail.com>
2023-01-02 19:47:40 -0800

    Complete custom todo app

--- todo-self-made/src/components/todos/TodoItem.js
+++ todo-self-made/src/components/todos/TodoItem.js
@@ -1,10 +1,10 @@
-// import React, {useState} from 'react'
 
+import './TodoItem.css'
 const TodoItem = (props) => {
-
 
     const completeSwitch = (event) => {
         event.preventDefault();
+        
         props.onSub(event.target[0].value.split(','))
     }
     const deleteTodo = (event) => {
@@ -12,21 +12,24 @@ const TodoItem = (props) => {
         props.onSub(event.target[0].value.split(','))
     }
     return(
-        <li>
-            <form onSubmit={completeSwitch}>
-                <h4>{props.text}</h4>
-                {props.bool === 'true' ? (
-                    <button type='submit' value={[props.bool, props.text, 'com']}>Incomplete</button>
-                    ):(
-                        <button type='submit' value={[props.bool, props.text, 'com']}>Complete</button>
-                        )
-                        
-                    }
-            </form>
-            <form onSubmit={deleteTodo}>
-                    <button type="submit" value={[props.bool, props.text, 'del']}>Delete</button>
-            </form>
-                <h5>Complete: {props.bool}</h5>
+        <li className={`todo-wrap ${props.bool.toString()}`}>
+            
+            <h4>{props.text}</h4>
+            <div>
+                <form onSubmit={completeSwitch}>
+                    {props.bool === 'true' ? (
+                        <button type='submit' value={[props.bool, props.text, 'com']}>Incomplete</button>
+                        ):(
+                            <button type='submit' value={[props.bool, props.text, 'com']}>Complete</button>
+                            )
+                            
+                        }
+                </form>
+                <form onSubmit={deleteTodo}>
+                        <button id="del-btn" type="submit" value={[props.bool, props.text, 'del']}>X</button>
+                </form>
+
+            </div>
         </li>
     )
 }
